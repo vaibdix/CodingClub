672d653c6c5c23c1c2f0a51148def7a6fafcbbfe
vaibdix <dixitvaib@gmail.com>
2023-09-04 04:24:05 +0530

    gallery experimental

--- js/main.js
+++ js/main.js
@@ -351,3 +351,29 @@ document.addEventListener('mouseup', handleMouseUp)
 document.addEventListener('touchstart', handleMouseDown)
 document.addEventListener('touchmove', handleMouseMove)
 document.addEventListener('touchend', handleMouseUp)
+
+
+
+
+const nav = document.getElementById("nav");
+
+for(const link of nav.getElementsByTagName("a")) {  
+  link.onmousemove = e => {
+    const rect = link.getBoundingClientRect(),    
+          img = link.querySelector("img");
+    
+    img.style.left = `${e.clientX - rect.left}px`;
+    img.style.top = `${e.clientY - rect.top}px`;
+  }
+}
+
+window.onmousemove = e => {
+  const percent = e.clientY / window.innerHeight;
+  
+  nav.animate({
+    transform: `translateY(${percent * nav.offsetHeight * -1}px)`
+  }, {
+    fill: "forwards",
+    duration: 4000
+  })
+}
